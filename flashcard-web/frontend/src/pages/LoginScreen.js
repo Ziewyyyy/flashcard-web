70726036542d849a9c40b1477e6c3b94ca1e8b58
Ziewyyyy <trinhanhc5@gmail.com>
2026-04-24 21:25:41 +0700

    Update LoginScreen.js

--- flashcard-web/frontend/src/pages/LoginScreen.js
+++ flashcard-web/frontend/src/pages/LoginScreen.js
@@ -1,56 +1,60 @@
-  import { useState, useEffect } from "react";
-  import { login } from "../api/authApi";
-  import { useNavigate } from "react-router-dom";
-  import { ToastContainer, toast } from "react-toastify";
-  import "react-toastify/dist/ReactToastify.css";
+import { useState, useEffect } from "react";
+import { login } from "../api/authApi";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-  export default function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    const API = process.env.REACT_APP_API_URL;
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const API = process.env.REACT_APP_API_URL;
 
-    useEffect(() => {
+  useEffect(() => {
 
-      const checkToken = async () => {
-        const token = localStorage.getItem("token");
-        if (!token) return;
+    const checkToken = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) return;
 
-        try {
-          const response = await fetch(`${API}/api/decks`, {
-            headers: {
-              "Authorization": `Bearer ${token}`
-            }
-          });
-
-          if (response.ok) {
-            navigate("/");
-          } else {
-            localStorage.removeItem("token");
+      try {
+        const response = await fetch(`${API}/api/decks`, {
+          headers: {
+            "Authorization": `Bearer ${token}`
           }
-        } catch (err) {
+        });
+
+        if (response.ok) {
+          navigate("/");
+        } else {
           localStorage.removeItem("token");
         }
-      };
-
-      checkToken();
-    }, [navigate, API]);
-
-    const handleLogin = async (e) => {
-      e.preventDefault();
-      try {
-        const res = await login({ username, password });
-        localStorage.setItem("token", res.data.token);
-        toast.success("Login success!");
-        setTimeout(() => navigate("/"), 1500);
-        navigate("/");
       } catch (err) {
-        console.error("Login failed", err);
-        toast.error("Login failed: " + (err.response?.data?.message || err.message));
+        localStorage.removeItem("token");
       }
     };
 
-    return (
+    checkToken();
+  }, [navigate, API]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login({ username, password });
+      console.log("LOGIN RESPONSE:", res);
+      localStorage.setItem("token", res.data.token);
+      toast.success("Login success!");
+      setTimeout(() => navigate("/"), 1500);
+      navigate("/");
+    } catch (err) {
+      console.error("Login failed", err);
+      toast.error("Login failed: " + (err.response?.data?.message || err.message));
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer />
+
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-800">
@@ -103,5 +107,6 @@
           </div>
         </div>
       </div>
-    );
-  }
+    </>
+  );
+}
