6836438faacd6d317a9d5da134b42baac83ac24a
Ziewyyyy <trinhanhc5@gmail.com>
2026-04-29 21:40:08 +0700

    Fix CORS configuration by adding missing comma

--- flashcard-web/backend/src/main/java/com/flashcard/flashcard_web/config/CorsConfig.java
+++ flashcard-web/backend/src/main/java/com/flashcard/flashcard_web/config/CorsConfig.java
@@ -3,29 +3,22 @@ package com.flashcard.flashcard_web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.*;
-
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowCredentials(true);
-
         config.setAllowedOrigins(List.of(
-                "https://anki-clone-v2.vercel.app"
-                "http://localhost:5173",
+                "https://anki-clone-v2.vercel.app",
+                "http://localhost:5173"
         ));
-
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("*"));
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return source;
     }
 }
