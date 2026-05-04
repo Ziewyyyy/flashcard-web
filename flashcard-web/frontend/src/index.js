0f4fe039fd1554628a008976eee68540675bca2d
Ziewyyyy <trinhanhc5@gmail.com>
2026-05-04 19:56:08 +0700

    thêm phần ngôn ngữ vào setting (đã sửa card và home screen)

--- flashcard-web/frontend/src/index.js
+++ flashcard-web/frontend/src/index.js
@@ -5,15 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StudyProvider } from "./context/StudyContext";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { LanguageProvider } from "./context/LanguageContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="591018459960-sbvbs143it9eoi2hl1k9ca0ms8gsvfln.apps.googleusercontent.com">
-      <StudyProvider>
-        <App />
-      </StudyProvider>
+      <LanguageProvider>
+        <StudyProvider>
+          <App />
+        </StudyProvider>
+      </LanguageProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
